597c1755d0fcb261368ba0adb3e2d140192830b7
samod singh <samod0098@gmail.com>
2023-02-06 11:48:07 +0530

    constractor function

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let a ={
-    fname : "Samod",
-    lname : "Singh",
-    age : 23,
-    email : "samod0098@gmail.com",
-    fevMovies : ["Dhoom", "Sholay", "Hum"],
-    salary : function(){
-        return 2500;
-    },
-    fullname : function(){
-        return this.fname + " " + this.lname;
-    }
-}
-console.log(a.fullname());
+// Constructor function------
+// let student = {
+//     firsName : "Samod",
+//     lastName : "singh",
+//     age : 23,
+//     class : 11
+// }
+// console.log(student);
 
+function Student(first, last, age, cls){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.class = cls;
+}
+    Student.prototype.nationality = "Indian";           // add proparty in constructor function----------
+
+let student1 = new Student("Samod", "Singh", 23, 11);
+                             // proparty add in obejct
+// student1.nationality = "Indian";
+
+console.log(student1);
