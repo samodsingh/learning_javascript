63e1d19a67d65b825956a67682c81b3723ad3aa4
samod singh <samod0098@gmail.com>
2023-02-07 11:28:39 +0530

    date methodes

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-// Array all methodes----------------------
+let now = new Date();
+// let date = now.toDateString();
 
-// add and remove item in array -------------
-let arr = ["samod", "umesh", "sudhir"];
+// console.log(now.getFullYear());
 
-arr.push("sunil"); // arr.push(...items) – adds items to the end,
-// arr.pop()// arr.pop() – extracts an item from the end,
-// console.log(arr);
-
-// arr.shift(); // extracts an item from the beginning,
-arr.unshift("Mohit");
- console.log(arr); 
-
-// splice---------------------------------------------
-//  arr.splice(1, 1); // from index 1 remove 1 element
-//  arr.splice(0, 2, "singh"); // remove 2 first elements and replace them with another
-
-// splice-------------------------------------------------------
-
-// let myarr =  arr.slice(1, 2); // copy from 1 to 2
-//  console.log(myarr);
-
-//Concat ------------------------------------------------------
-let num = [1, 2, 3];
-let add = num.concat([4, 5]);
-// console.log(add);
-
-// Iterate: forEach--------------------------------------------
-arr.forEach((item, index) =>{
-    // alert(item);
-}
-)
-
-
+let today = new Date(2022, 01, 02);
+// today.setFullYear(2020 , 01, 02);
+// today.setDate(today.getDate() + 3);
+console.log(today);
