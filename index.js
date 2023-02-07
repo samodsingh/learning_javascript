1261262571805b0e9b09233dd1f1311d482171b2
samod singh <samod0098@gmail.com>
2023-02-07 12:32:17 +0530

    Json stringify and parse

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-let now = new Date();
-// let date = now.toDateString();
+let site = {
+    name: 'W3Docs',
+    hasAdmin: true,
+    books: ['html', 'css', 'js', 'git'],
+    booksCount: 4,
+  };
 
-// console.log(now.getFullYear());
+  console.log(typeof site);
+  let json = JSON.stringify(site);//JSON.stringify to convert objects into JSON.
+  console.log(typeof json); // we've got a string!
+  console.log(json);
 
-let today = new Date(2022, 01, 02);
-// today.setFullYear(2020 , 01, 02);
-// today.setDate(today.getDate() + 3);
-console.log(today);
+
+  let myJson = {
+    name : "Samod",
+    lname : "Singh",
+    age : 23
+};
+  console.log(JSON.stringify(myJson));
+
+let userData = '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+
+let user = JSON.parse(userData); //JSON.parse to convert JSON back into an object.
+
+console.log( user);
